9fa93259d856e3accb5ab1a60ceb155a416e38b9
Rahmat Ilyas <63534535+Rahmat-Ilyas@users.noreply.github.com>
2020-07-13 18:09:03 +0800

    Update koneksi.php

--- koneksi/koneksi.php
+++ koneksi/koneksi.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect("localhost", "root", "", "madani");
+$conn = mysqli_connect("db4free.net", "rahmatilyas", "14-02-1998", "db_apotek");
 
 $datareset = mysqli_query($conn, "SELECT * FROM tb_resep WHERE status='Belum Acc'");
 $notif=[];
